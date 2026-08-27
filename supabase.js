aa176e3eb85c8db9f2d99d409f53a21733f36f7b
rejoiceabumere <ytr2h4hh9f@privaterelay.appleid.com>
2026-08-27 17:10:28 +0100

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,9 @@
+const SUPABASE_URL = "https://laxbtgmzrqfxwhysevtt.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_EYvaNqNFMfR8yZ1FPgeiqg_ferNURTH";
+
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
