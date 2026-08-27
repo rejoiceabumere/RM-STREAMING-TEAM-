d5d171dabebb2dd932e11755df383bc5a6f6424a
rejoiceabumere <ytr2h4hh9f@privaterelay.appleid.com>
2026-08-27 19:36:22 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
-const supabaseUrl = "https://laxbtgmzrqfxwhysevtt.supabase.co";
+const supabaseUrl = "https://laxbtgmzrqfxwhysevtt.supabase.co/rest/v1/";
 
 const supabaseAnonKey = "sb_publishable_EYvaNqNFMfR8yZ1FPgeiqg_ferNURTH";
 
